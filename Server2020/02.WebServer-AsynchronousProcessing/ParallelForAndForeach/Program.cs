b91fa00b2018d3d54edf9187419e4a38d90636d9
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-02-19 10:54:50 +0200

    Add new functionality

---
+++ Server2020/02.WebServer-AsynchronousProcessing/ParallelForAndForeach/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ParallelForAndForeach
+{
+    class Program
+    {
+        static int Count = 0;
+        static object lockObj = new object();
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            PrintPrimeCount(1, 10_000);
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        static void PrintPrimeCount(int min, int max)
+        {
+            //synchronous:
+            //for (int i = min; i <= max; i++)
+
+            //parallel For:
+            Parallel.For(min, max + 1, i =>
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                    if (isPrime)
+                    {
+                        lock (lockObj)
+                        {
+                            Count++;
+                        }
+                    }
+                }
+
+            });
+            Console.WriteLine(Count);
+        }
+    }
+}
