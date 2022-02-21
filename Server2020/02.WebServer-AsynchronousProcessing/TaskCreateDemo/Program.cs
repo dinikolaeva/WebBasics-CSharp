33cb08b02a21059e7639d62f574f454b01466bd8
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-02-21 18:11:23 +0200

    TaskCreateDemo

---
+++ Server2020/02.WebServer-AsynchronousProcessing/TaskCreateDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskCreateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                var task = Task.Run(async () =>
+                {
+                    HttpClient httpClient = new HttpClient();
+
+                    var url = $"https://vicove.com/vic-{i}";
+
+                    var httpResponse = await httpClient.GetAsync(url);
+                    var vic = await httpResponse.Content.ReadAsStringAsync();
+                    Console.WriteLine(vic.Length);
+                });
+
+                tasks.Add(task);
+            }
+
+            //wait all task to finish
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
