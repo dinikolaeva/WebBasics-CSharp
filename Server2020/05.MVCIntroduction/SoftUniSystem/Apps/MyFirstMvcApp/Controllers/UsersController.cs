d07e924a2c042728270a7650d1a4c1fe03751d0e
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-02-27 17:58:00 +0200

    MyFirstMVCApp

---
+++ Server2020/05.MVCIntroduction/SoftUniSystem/Apps/MyFirstMvcApp/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System.Text;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login(HttpRequest request)
+        {
+            return this.View();
+        }
+
+        public HttpResponse Register(HttpRequest request)
+        {
+           return this.View();
+        }
+
+        public HttpResponse DoLogin(HttpRequest request)
+        {
+            return this.Redirect("/");
+        }
+    }
+}
---
+++ Server2020/05.MVCIntroduction/SoftUniSystem/Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyFirstMvcApp.Controllers;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           await Host.CreateHostAsync(new Startup());
+        }
+    }
+}
