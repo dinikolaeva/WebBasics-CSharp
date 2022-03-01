74441d9d43b57a27ff06272166b7380ce2ec53d6
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-03-01 10:33:01 +0200

    Add MVC Framework

---
+++ Server2020/05.MVCIntroduction/SoftUniSystem/SUS.MvcFramework/Controller.cs
@@ -0,0 +1,42 @@
+﻿using SUS.HTTP;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SUS.MvcFramework
+{
+    public abstract class Controller
+    {
+        public HttpResponse View([CallerMemberName] string viewPath = null)
+        {
+            var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
+
+            var viewContent = System.IO.File
+                                        .ReadAllText("Views/" +
+                                        this.GetType().Name
+                                        .Replace("Controller", string.Empty) +
+                                        "/" + viewPath +
+                                        ".cshtml");
+
+            var responseHtml = layout.Replace("@RenderBody()", viewContent);
+
+            var responseBodyBites = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBites);
+
+            return response;
+        }
+
+        public HttpResponse File(string pathFile, string contentType)
+        {
+            var fileBytes = System.IO.File.ReadAllBytes(pathFile);
+            var response = new HttpResponse(contentType, fileBytes);
+            return response;
+        }
+
+        public HttpResponse Redirect(string url)
+        {
+            var response = new HttpResponse(HttpStatusCode.Found);
+            response.Headers.Add(new Header("Location", url));
+            return response;
+        }
+    }
+}
