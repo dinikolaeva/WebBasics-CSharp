11c2356f681eead712d8ad0fc5b73e9f5d9e8fa9
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-03-20 18:54:55 +0200

    Add SUS.HTTP

---
+++ Server2020/09.Workshop3/BattleCards(Exam28.04.2020)/SUS.HTTP/HttpConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SUS.HTTP
+{
+    public static class HttpConstants
+    {
+        public const string NewLine = "\r\n";
+        public const string RequestCookieHeader = "Cookie";
+        public const string SessionCookieName = "SUS_SID";
+    }
+}
