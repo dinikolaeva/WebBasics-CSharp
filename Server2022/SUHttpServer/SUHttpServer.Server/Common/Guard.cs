faf2ff07d5ab1de35477a19a52b36772ad77ce6e
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-04-30 15:14:18 +0300

    Add server common files

---
+++ Server2022/SUHttpServer/SUHttpServer.Server/Common/Guard.cs
@@ -0,0 +1,15 @@
+﻿namespace SUHttpServer.Server.Common
+{
+    public class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                name ??= "Value";
+
+                throw new ArgumentException($"{name} can not be null.");
+            }
+        }
+    }
+}
