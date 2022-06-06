9a1ed77fde564c5d4128816fb2882e43277d8084
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-06-06 11:12:57 +0300

    Add Http files

---
+++ Server2022/ExamPreparation/Exam-CarShop(23.12.2020)/Solution/MyWebServer/Identity/UserIdentity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyWebServer.Identity
+{
+    public class UserIdentity
+    {
+        public string Id { get; init; }
+
+        public bool IsAuthenticated => this.Id != null;
+    }
+}
