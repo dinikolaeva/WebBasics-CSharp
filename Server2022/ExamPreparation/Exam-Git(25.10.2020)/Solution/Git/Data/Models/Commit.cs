35cd692a27ea351d86a6f6f74b405099dbf3ac2f
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-07-01 09:57:30 +0300

    Add models

---
+++ Server2022/ExamPreparation/Exam-Git(25.10.2020)/Solution/Git/Data/Models/Commit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Git.Data.Models
+{
+    public class Commit
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required]
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string CreatorId { get; set; }
+        public User Creator { get; set; }
+        public string RepositoryId { get; set; }
+        public Repository Repository { get; set; }
+    }
+}
---
+++ Server2022/ExamPreparation/Exam-Git(25.10.2020)/Solution/Git/Data/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Git.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public IEnumerable<Repository> Repositories { get; set; } = new HashSet<Repository>();
+        public IEnumerable<Commit> Commits { get; set; } = new HashSet<Commit>();
+    }
+}
